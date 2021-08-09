98442ac5c234c209cc49ebfcfa1dba03078e62d0
Prem <premsagar.choudhary@mroads.com>
2021-08-09 15:10:20 +0530

    avl

--- Tree/AVL Tree/AVLTree.cpp
+++ Tree/AVL Tree/AVLTree.cpp
@@ -1,18 +1,18 @@
 #include<iostream>
 using namespace std;
 
-struct node {
+typedef struct node {
   int key;
   struct node *left;
   struct node *right;
   int height;
-};
+}Node;
 
 int max(int x, int y);
-int height(struct node *N);
+int height(Node* N);
 
 //function to get the height of the tree
-int height(struct node *N) {
+int height(Node* N) {
   if (N == NULL)
     return 0;
   return N->height;
@@ -24,11 +24,42 @@ int max(int x, int y) {
 }
 
 //function to create a node with given key
-struct node *createNode(int key) {
-  struct node *newNode = (struct node*)malloc(sizeof(struct node));
+Node* createNode(int key) {
+  Node* newNode = (Node*)malloc(sizeof(Node));
   newNode->key = key;
   newNode->left = NULL;
   newNode->right = NULL;
   newNode->height = 1; //new node is initially added at leaf
   return newNode;
+}
+
+Node* leftRotate(Node* x) {
+  Node* y = x->right;
+  Node* T2 = y->left;
+
+  //perform left rotation
+  y->left = x;
+  x->right = T2;
+
+  return y;
+}
+
+Node* rightRotate(Node* z) {
+  Node* y = z->left;
+  Node* T2 = y->right;
+
+  //perform right rotation
+  y->right = z;
+  z->left = T2;
+
+  return y;
+}
+
+Node* insert() {
+  
+}
+
+int main() {
+  Node* root = NULL;
+  return 0;
 }
