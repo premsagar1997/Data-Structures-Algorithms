9dcf615f90dcc771693a66a1b1c07733ad9df411
Premsagar Choudhary <76701517+premsagar1997@users.noreply.github.com>
2022-09-29 15:46:31 +0530

    Update BST.cpp

--- Tree/Binary Search Tree(BST)/ClassImplementationOfBST/BST.cpp
+++ Tree/Binary Search Tree(BST)/ClassImplementationOfBST/BST.cpp
@@ -1,3 +1,4 @@
+// Online C++ compiler to run C++ program online
 #include <iostream>
 using namespace std;
 
@@ -35,6 +36,7 @@ class BST {
     void insertIterative(Tree *newNode) {
         if (root == NULL) { //no node present in tree
             root = newNode;
+            cout<<"Inserted as root"<<endl;
         } else {
             Tree *temp = root;
             while (temp != NULL) {
@@ -42,24 +44,52 @@ class BST {
                     return;
                 } else if (newNode->key < temp->key && temp->left == NULL) {
                     temp->left = newNode;
+                    cout<<"Inserted as left child"<<endl;
                     break;
                 } else if (newNode->key < temp->key) {
                     temp = temp->left;
                 } else if (newNode->key > temp->key && temp->right == NULL) {
                     temp->right = newNode;
+                    cout<<"Inserted as right child"<<endl;
+                    break;
                 } else {
                     temp = temp->right;
                 }
             }
         }
     }
+    
+    void preOrder(Tree *r) {
+        if (r == NULL)
+            return;
+        cout<<r->key<<" ";
+        preOrder(r->left);
+        preOrder(r->right);
+    }
+    
+    void inOrder(Tree *r) {
+        if (r == NULL)
+            return;
+        inOrder(r->left);
+        cout<<r->key<<" ";
+        inOrder(r->right);
+    }
+    
+    void postOrder(Tree *r) {
+        if (r == NULL)
+            return;
+        postOrder(r->left);
+        postOrder(r->right);
+        cout<<r->key<<" ";
+    }
 };
 
 int main() {
     BST bstObj;
     int option, key;
-    
+
     do {
+        cout<<"1->Insert\n2->Print PreOrder\n3->Print InOrder"<<endl;
         cin>>option;
         switch (option) {
             case 0: {
@@ -69,8 +99,19 @@ int main() {
             case 1: {
                 cout<<"Enter element to insert:- ";
                 cin>>key;
-                Tree *newNode = new Tree(key);
+                Tree *newNode = new Tree();
+                newNode->key = key;
                 bstObj.insertIterative(newNode);
+                break;
+            }
+            case 2: {
+                bstObj.preOrder(bstObj.root);
+                cout<<endl;
+                break;
+            }
+            case 3: {
+                bstObj.inOrder(bstObj.root);
+                cout<<endl;
                 break;
             }
             default:
